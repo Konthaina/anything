cff1c4b1ab267fe14d3ccf0cfeab463024cf5141
Konthaina <thainakon87@gmail.com>
2025-12-15 17:24:59 +0700

    add button scroll to top

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -59,7 +59,8 @@ export function AppSidebar() {
                 <SidebarMenu>
                     <SidebarMenuItem>
                         <SidebarMenuButton size="lg" asChild>
-                            <Link href={dashboard()} prefetch>
+                            {/* <Link href={dashboard()} prefetch> */}
+                            <Link href='/feed' prefetch>
                                 <AppLogo />
                             </Link>
                         </SidebarMenuButton>
